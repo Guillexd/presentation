a3a37efb63c51a2d5d83f7c96649c1000184af4f
GuillexD <enrique2003junior@gmail.com>
2023-01-22 00:19:06 -0500

    Finally...

--- js/index.js
+++ js/index.js
@@ -1,6 +1,38 @@
-// header cambie de color 
-// opacity en el home para efecto pro xd
+
 AOS.init({
     duration: 1500,
     once: true
 });
+
+d=document;
+d.addEventListener('click', (e)=>{
+  if(e.target.matches('.examples *') && window.innerWidth<1000){
+    const Redirection = e.target.closest('figure').lastElementChild.lastElementChild.lastElementChild.getAttribute
+    ('href');
+    e.target.closest('section').innerHTML+=`<meta http-equiv="refresh" content="0; url=${Redirection}">`;
+  }
+
+  if(e.target.matches('#menu *') && window.innerWidth<1000){
+    e.target.closest('div').classList.remove('show');
+  }
+})
+
+const titles = d.querySelectorAll('h2');
+const titles__navbar = d.querySelectorAll('#menu li a');
+
+const watchTitles = (entries, observer)=>{
+  entries.forEach(element => {
+    if(element.isIntersecting){
+      titles__navbar.forEach(el=>{
+        if(el.textContent == element.target.textContent){
+          el.classList.add('style_menu__list')
+          console.log(el);
+        } else {
+          el.classList.remove('style_menu__list')
+        }
+      })
+    }
+  });
+}
+const observator = new IntersectionObserver(watchTitles);
+titles.forEach(el=>observator.observe(el));
